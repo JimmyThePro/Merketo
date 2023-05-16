610e72cede2a5aaf3151dcf47dcb649dcc51faff
Jimmy <jimmyjunttila@hotmail.com>
2023-05-16 15:58:56 +0200

    Add bootstrap/fontawesome/HomeController.cs and Sass files for styling

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Merketo.Controllers;
+
+public class HomeController : Controller
+{
+    public async Task<IActionResult> Index()
+    {
+        return View();
+    }
+}
