2d877b48be956ccdcd8e0b41492c40a015f25f7d
Jimmy <jimmyjunttila@hotmail.com>
2023-05-23 13:29:40 +0200

    Add ProductController with Index action and routing

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Merketo.Helpers.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Merketo.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly ProductRepository _productRepository;
+
+    public ProductController(ProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    [HttpGet]
+    [Route("/products")]
+    public async Task<IActionResult> Index()
+    {
+        var products = await _productRepository.GetAllAsync();
+        return View(products);
+    }
+}
