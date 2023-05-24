b648bc586e3d269a53814e9cae917c3714c2e530
Jimmy <jimmyjunttila@hotmail.com>
2023-05-24 11:29:26 +0200

    Add UserSignUpViewModel and a SignUp form, also add AccountController with Index/SignUp actions

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Merketo.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Merketo.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(UserSignUpViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(viewModel);
+        }
+    }
+}
---
+++ Models/ViewModels/UserSignUpViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Merketo.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Merketo.Models.ViewModels;
+
+public class UserSignUpViewModel
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; } = null!;
+
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
+
+    [DataType(DataType.Password)]
+    [Compare(nameof(Password))]
+    public string ConfirmPassword { get; set; } = null!;
+    public string? PhoneNumber { get; set; }
+    public string? StreetName { get; set; }
+    public string? PostalCode { get; set; }
+    public string? City { get; set; }
+
+    public static implicit operator IdentityUser(UserSignUpViewModel viewModel)
+    {
+        return new IdentityUser
+        {
+            UserName = viewModel.Email,
+            Email = viewModel.Email,
+            PhoneNumber = viewModel.PhoneNumber
+        };
+    }
+
+    public static implicit operator UserProfileEntity(UserSignUpViewModel viewModel)
+    {
+        return new UserProfileEntity
+        {
+            FirstName = viewModel.FirstName,
+            LastName = viewModel.LastName,
+            StreetName = viewModel.StreetName,
+            PostalCode = viewModel.PostalCode,
+            City = viewModel.City
+        };
+    }
+}
